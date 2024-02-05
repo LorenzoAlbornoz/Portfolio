3c8f8a9c7b48db91fed6ad62561e9d854e9639ad
Lorenzo Albornoz <lorenzoa.28.la@gmail.com>
2024-02-05 12:12:28 -0300

    proyectos y otros avances

---
+++ src/components/Proyectos/Proyectos.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import CardProject from '../CardProject/CardProject'
+
+const Proyectos = () => {
+  return (
+    <div className='container-fluid my-5' id='projects'>
+        <div className="row">
+            <div className='py-4 text-center'>
+            <h2>Proyectos</h2>
+            <h5>Aqui puedes ver algunos de los proyectos en los que trabajé</h5>
+            </div>
+            <div className="col d-flex justify-content-around py-5 flex-wrap gap-3">
+                
+            <CardProject 
+                title={'Informate'} 
+                text={'Diseño web completamente responsive de un diario online utilizando HTML, CSS y Bootstrap 5.2.3 para garantizar una experiencia óptima en diferentes dispositivos.'}
+                site={'https://proyectogrupo5.netlify.app/'}
+                repo={'https://github.com/LorenzoAlbornoz/ProyectoBoostrap'}
+                img={'https://res.cloudinary.com/dcwpf7ghu/image/upload/v1707143984/informate_f6aite.png'}
+            />
+            <CardProject 
+                title={'MusicFlow'} 
+                text={' Sitio web para administración y reproduccion de musica. Se usó el localstorage como almacenamiento de los crud y las funciones escritas con javascript nativo'}
+                site={'https://music-flow.netlify.app'}
+                repo={'https://github.com/LorenzoAlbornoz/ProyectoJavaScript'}
+                img={'https://res.cloudinary.com/dcv6aut2v/image/upload/v1700349891/Porfolio/MusicFlow_no8plg.png'}
+            />
+            <CardProject 
+                title={'LAM Music Store'} 
+                text={'Sitio web construido en React y Bootstrap, esta plataforma incorpora Express y MongoDB. Ofrece una experiencia completa con interfaz atractiva y sólida gestión del backend.'}
+                site={'https://lam-music.netlify.app/'}
+                repo={'https://github.com/Fsalasgit/LAM-music-store'}
+                img={'https://res.cloudinary.com/dcwpf7ghu/image/upload/v1707143334/LAM-MUSIC-STORE_ku1bld.png'}
+            />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Proyectos
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import NavbarM from '../components/Navbar/Navbar'
 import Hero from '../components/Hero/Hero'
 import About from '../components/About/About'
+import Habilidades from '../components/Habilidades/Habilidades'
 import Servicios from '../components/Servicios/Servicios'
+import Proyectos from '../components/Proyectos/Proyectos'
 import Contacto from '../components/Contacto/Contacto'
 import Footer from '../components/Footer/Footer'
 
@@ -13,7 +15,9 @@ const Home = () => {
       <NavbarM />
       <Hero />
       <About />
+      <Habilidades/>
       <Servicios/>
+      <Proyectos />
       <Contacto/>
       <Footer />
     </>
